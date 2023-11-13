28a0058051880ce1288ecd1f0ad57b461271720c
Theres Palatty <therespalatty@gmail.com>
2023-11-13 18:20:39 +0100

    modified code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,102 @@
 import random
 
 
-def function_A(min, max):
+def get_random_integer(min, max):
     """
-    Random integer.
+
+    This function takes two parameters min and max 
+    and return a random integer between min and max 
+
+    Parameter:
+    integer (min)
+    integer (max)
+
+    Return:
+    integer
+
     """
     return random.randint(min, max)
 
 
-def function_B():
+def get_operation():
+    """ 
+    This function chooses an operation among '+' '-' and '*'
+    
+    Parameters:
+    None
+
+    Return:
+    string
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def Operation(Num_1, Num_2, o):
+    """
+    This function takes three parameters 
+    and computes the necessary operation 
+
+    Parameters:
+    Integer (Num_1)
+    Integer (Num_2)
+    string (o)
+
+    Return:
+    String (p)
+    Integer (a)
+
+    """
+
+    p = f"{Num_1} {o} {Num_2}"
+    if o == '+': 
+        a = Num_1 + Num_2
+    elif o == '-': 
+        a = Num_1 - Num_2
+    else: 
+        a = Num_1 * Num_2
+    return p,a
 
 def math_quiz():
-    s = 0
-    t_q = 3.141592653599
+    """
+    This function conducts Maths quiz and display result
 
-    print("Welcome to  the Math Quiz Game!")
+    Parameter:
+    None
+
+    Return:
+    None
+    """
+    Score = 0
+    t_q = 5
+
+    print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
-
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
+        Num_1 = get_random_integer(1, 10)
+        Num_2 = get_random_integer(1, 54)
+        o = get_operation()
+        question, result = Operation(Num_1, Num_2, o)
+        print(f"\nQuestion: {question}")
+        try:
+            userinput = input("Your answer: ")
+            userinput = int(userinput)  # Try converting the input to a int
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Valid input! You entered: {userinput}")
+        
+    
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+        
 
-if __name__ == "__main__":
+        if userinput == result:
+            print("Correct! You earned a point.")
+            Score += 1
+        else:
+            print(f"Wrong answer. The correct answer is {result}.")
+
+    print(f"\nGame over! Your score is: {Score}/{t_q}")
+
+if __name__ == "_main_":
     math_quiz()
